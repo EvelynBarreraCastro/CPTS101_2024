f6e86b7029b9be9b51847ad46778a32316ef3ec2
Evelyn Barrera <e.barreracastro@wsu.edu>
2024-09-10 14:35:36 -0700

    Create helloworld.c
    simple program

---
+++ helloworld.c
@@ -0,0 +1,16 @@
+/*  
+    Programmer: Evelyn Barrera Castro
+    Date: September 10, 2024
+    Compiler: Visual Studio Code
+    Purpose: To write a simple C Program
+*/ 
+
+#include <stdio.h> 
+
+int main () 
+{ 
+ printf("Hello World!");  
+
+return 0; 
+
+}
